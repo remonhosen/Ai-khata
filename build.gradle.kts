f46d85e749e79edeec41f2176c0cd1484c09b0f9
remonhosen <remonhosen@yahoo.com>
2026-09-13 23:44:24 +0600

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,30 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.remon.hisab"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.remon.hisab"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
